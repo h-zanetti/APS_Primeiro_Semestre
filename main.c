2a65e5934ab7f9c382bef01a71d980e2b083f625
agah <henriaguiar99@gmail.com>
2020-11-03 15:48:55 -0200

    Finalizado

--- main.c
+++ main.c
@@ -23,41 +23,84 @@ int main(void) {
   // Recebendo vetor 2
   preencher_vetor(vetor2);
 
-  // Retornando Vetores
-  printf("Vetor 1 = [");
-  for(int i=0; i<TAMANHO_VETOR-1; i++) {
-    printf("%d, ", vetor1[i]);
-  }
-  printf("%d]\n", vetor1[TAMANHO_VETOR-1]);
-
-  printf("Vetor 2 = [");
-  for(int i=0; i<TAMANHO_VETOR-1; i++) {
-    printf("%d, ", vetor2[i]);
-  }
-  printf("%d]\n", vetor2[TAMANHO_VETOR-1]);
-
-  // Gerenciando operações
-  char escolha = ' ';
-  printf("Digite '+' para soma\n");
-  printf("Digite '-' para subtração\n");
-  printf("Digite '*' para multiplicação\n");
-
+  // Repetir operações
+  char comando = ' ';
   do {
-    printf("\nEscolha uma operação: ");
-    scanf(" %c", &escolha);
-    if(escolha == '+') {
-      resultado = soma(vetor1, vetor2);
-    } else if (escolha == '-') {
-      resultado = subtracao(vetor1, vetor2);
-    } else if (escolha == '*') {
-      resultado = multiplicacao(vetor1, vetor2);
-    } else {
-      printf("Operação inválida\n");
+    // Retornando Vetores
+    printf("\nVetor 1 = [");
+    for(int i=0; i<TAMANHO_VETOR-1; i++) {
+      printf("%d, ", vetor1[i]);
     }
-  } while(escolha != '+' && escolha != '-' && escolha != '*');
+    printf("%d]\n", vetor1[TAMANHO_VETOR-1]);
 
-  // Resultado
-  printf("Resultado: %d\n", resultado);
+    printf("Vetor 2 = [");
+    for(int i=0; i<TAMANHO_VETOR-1; i++) {
+      printf("%d, ", vetor2[i]);
+    }
+    printf("%d]\n", vetor2[TAMANHO_VETOR-1]);
+
+    // Gerenciando operações
+    char operacao = ' ';
+    printf("Digite '+' para soma\n");
+    printf("Digite '-' para subtração\n");
+    printf("Digite '*' para multiplicação\n");
+
+    do {
+      printf("\nEscolha uma operação: ");
+      scanf(" %c", &operacao);
+      if(operacao == '+') {
+        resultado = soma(vetor1, vetor2);
+      } else if (operacao == '-') {
+        resultado = subtracao(vetor1, vetor2);
+      } else if (operacao == '*') {
+        resultado = multiplicacao(vetor1, vetor2);
+      } else {
+        printf("Operação inválida\n");
+      }
+    } while(operacao != '+' && operacao != '-' && operacao != '*');
+
+    // Resultado
+    printf("Resultado: %d\n", resultado);
+
+    // Defenir repetição
+    printf("\nPara finalizar o programa digite 'x'\n");
+
+    do {
+      printf("Você deseja realisar outra operação com os mesmos vetores (s/n)? ");
+      scanf(" %c", &comando);
+
+      if (comando == 's') {
+        continue;
+
+      } else if (comando == 'n'){
+        printf("Qual vetor você deseja alterar? Digite 'u' para o vetor 1, 'd' para o vetor 2 e 'a' para ambos... ");
+        scanf(" %c", &comando);
+        if (comando == 'u') {
+          printf("Digite os valores do primeiro vetor:\n");
+          preencher_vetor(vetor1);
+          comando = 's';
+        } else if (comando == 'd') {
+          printf("Digite os valores do segundo vetor:\n");
+          preencher_vetor(vetor2);
+          comando = 's';
+        } else if (comando == 'a') {
+          printf("Digite os valores do primeiro vetor:\n");
+          preencher_vetor(vetor1);
+          printf("Digite os valores do segundo vetor:\n");
+          preencher_vetor(vetor2);
+          comando = 's';
+        } else {
+          printf("Comando inválido\n");
+        }
+
+      } else if (comando == 'x') {
+        break;
+
+      } else {
+        printf("Comando inválido\n");
+      }
+    } while(comando != 's' && comando != 'n' && comando != 'x');
+  } while(comando != 'x');
 
   return 0;
 }
